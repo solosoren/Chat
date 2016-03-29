9a2e20ca69a262fa2ffd123553b3457d3a35c806
Soren Nelson <sorennelson33@gmail.com>
2016-03-29 16:26:50 -0600

    HomeMessageCell

---
+++ Chat/Chat/HomeMessageCell.swift
@@ -0,0 +1,18 @@
+//
+//  HomeMessageCell.swift
+//  Chat
+//
+//  Created by Soren Nelson on 3/29/16.
+//  Copyright © 2016 SORN. All rights reserved.
+//
+
+import UIKit
+
+class HomeMessageCell: UITableViewCell {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var messageText: UITextView!
+    @IBOutlet weak var messageTime: UILabel!
+    
+    
+}
---
+++ Chat/Chat/HomeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HomeTableViewController.swift
+//  Chat
+//
+//  Created by Soren Nelson on 3/29/16.
+//  Copyright © 2016 SORN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("conversationCell", forIndexPath: indexPath) as! HomeMessageCell
+        return cell
+        
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+}
