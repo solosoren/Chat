52d9bd0183cd3633e576dfac9e60b18c1699d733
Soren Nelson <sorennelson33@gmail.com>
2016-03-29 15:32:32 -0600

    Initial Commit

---
+++ Chat/Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Soren Nelson on 3/29/16.
+//  Copyright © 2016 SORN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
