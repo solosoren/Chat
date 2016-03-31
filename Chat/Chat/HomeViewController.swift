34f36d7f728ba369b23797bdc65bd070698ca9ce
Soren Nelson <sorennelson33@gmail.com>
2016-03-30 18:52:06 -0600

    Home Segmented Control Setup

--- Chat/Chat/HomeViewController.swift
+++ Chat/Chat/HomeViewController.swift
@@ -11,17 +11,35 @@ import UIKit
 
 class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("conversationCell", forIndexPath: indexPath) as! HomeMessageCell
-        return cell
-        
-    }
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
     
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+    @IBAction func segmentedControlChanged(sender: AnyObject) {
+        tableView.reloadData()
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 100
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        if segmentedControl.selectedSegmentIndex == 0 {
+            let cell = tableView.dequeueReusableCellWithIdentifier("conversationCell", forIndexPath: indexPath) as! HomeMessageCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCellWithIdentifier("contactCell", forIndexPath: indexPath)
+            cell.textLabel?.text = "it works"
+            return cell
+        }
+    
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+
 }
+
+
